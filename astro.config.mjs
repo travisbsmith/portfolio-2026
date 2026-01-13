9e96aa4ba1f10308215ebd6ccc8c72dcd2607c59
Travis Smith <travissmith@Mac.localdomain>
2026-01-13 17:40:42 -0600

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fully-operational.com',
+  site: 'https://travisbsmith.github.io',
+  base: '/portfolio-2026',
   output: 'static',
   build: {
     assets: '_assets'
